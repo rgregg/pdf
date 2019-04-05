e12ffe1b582fadd695834f79db885ad1d22cdaf9
Ryan Gregg <ryangregg@google.com>
2019-04-04 22:06:45 -0700

    Pi calc

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,12 @@ FROM python:3-alpine
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 
-RUN apk add libreoffice \
-	build-base \ 
-	# Install fonts
-	msttcorefonts-installer fontconfig && \
-    update-ms-fonts && \
-    fc-cache -f
+RUN apk add bc gcc linux-headers musl-dev
 
+
+# RUN pip install --no-binary gevent gevent
+RUN pip install setuptools
 RUN pip install Flask requests gevent
 COPY . $APP_HOME
 
-CMD ["python", "to-pdf.py"]
+CMD ["python", "calc-pi.py"]
---
+++ calc-pi.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+import requests
+import subprocess
+
+from gevent.pywsgi import WSGIServer
+from flask import Flask, after_this_request, render_template, request, send_file
+from subprocess import Popen, PIPE, STDOUT
+
+app = Flask(__name__)
+
+
+def calculate_pi(digits):
+    input = 'scale=' + str(digits) + '; 4*a(1)\n'
+    cmd = ['/usr/bin/bc -l']
+    p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE, text=True, shell=True)
+    out, err = p.communicate(input)
+    if err:
+        print('error calculating: ', err)
+
+    translation_table = dict.fromkeys(map(ord, '\\'), None)
+    out = out.translate(translation_table)
+    return out
+
+
+@app.route('/', methods=['GET', 'POST'])
+def api():
+    digits = 100
+    calculate = False
+    value = None
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'digits' not in request.form:
+            return 'Number of digits was missing'
+        else:
+            digits = int(request.form['digits'])
+            calculate = True
+            if digits > 10000:
+                return 'Number of digits requested is too high. Try less than 10,000.'
+
+    if request.method == 'GET':
+        parm = request.args.get('digits', type=str)
+        if parm:
+            digits = int(parm)
+            if digits > 10000:
+                return 'Number of digits requested is too high. Try less than 10,000.'
+            calculate = True
+    
+    value = None
+    if calculate:
+        print('calculating pi to ', digits)
+        value = calculate_pi(digits)
+
+    return render_template('index.html', digits=digits, value=value)
+
+if __name__ == "__main__":
+    http_server = WSGIServer(('', int(os.environ.get('PORT', 8080))), app)
+    http_server.serve_forever()
